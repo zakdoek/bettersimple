58a0a394fed60685b5334921193bebaf28216a24
Tim Schilling <schilling711@gmail.com>
2013-10-21 21:40:50 -0400

    Getting the solution to be runnable again.

--- bettersimple/settings.py
+++ bettersimple/settings.py
@@ -97,9 +97,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    #os.path.join(PROJECT_PATH, 'static'),
-    '/home/schillingt/Dropbox/Projects/bettersimple/static',
-    '/app/static'
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -140,8 +138,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/schillingt/Dropbox/Projects/bettersimple/templates',
-    '/app/templates'
+    os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
